f534eb11f57523babe35e98dab7377fce50f9a82
PanfilovDenis <panf.denis@gmail.com>
2013-01-12 10:19:22 +0000

    add key value

--- lib/conf/builder.rb
+++ lib/conf/builder.rb
@@ -1,3 +1,17 @@
-class Builder
+module Conf
+  class Builder
+  	def self.build(env, block)
+  		b = new(env, block)
+    end
 
+    def initialize(env, block)
+    	@hash = {}
+    	instance_eval &block
+    end
+
+  	def env(name, &block)
+  	  @hash[name] = Proxy.build(block)
+  	end
+
+  end
 end
